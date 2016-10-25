219498f2fed20969d64b6d75f01358d0ebe0c594
IJedutest <ijobling@applicationcraft.com>
2016-10-25 08:16:54 +0000

    update

--- challenges/abc2.py
+++ challenges/abc2.py
@@ -1,7 +1,11 @@
 # Inputs
 import sys
-A = float(sys.argv[2])
-B = float(sys.argv[3])
-C = float(sys.argv[4])
+A = float(sys.argv[1])
+B = float(sys.argv[2])
+C = float(sys.argv[3])
 
 # Write your code 
+num = A * (B + C/3)
+
+# Outputs
+print( num )
